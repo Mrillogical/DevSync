90d0161e045a4103f17040e338ddc80ff4c5e26a
Kunal Singh Yadav <110709148+Mrillogical@users.noreply.github.com>
2026-05-13 11:36:48 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,7 +4,7 @@ export const STORAGE_KEY = 'devsync_token';
 
 /** @type {import('axios').AxiosInstance} */
 export const api = axios.create({
-  baseURL: 'devsync-production-998b.up.railway.app',
+  baseURL: 'devsync-production-998b.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
